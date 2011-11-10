7676674c1b0a1126542147c62784d23b01fcec43
guillaumepoiriermorency@gmail.com <guillaumepoiriermorency@gmail.com@88543d1f-1afb-2b75-7ae9-38cb9dc2246a>
2011-11-10 04:24:06 +0000

    Beaucoup de caca

--- branches/main-release/src/jngfjkngfgf/Jngfjkngfgf.java
+++ branches/main-release/src/jngfjkngfgf/Jngfjkngfgf.java
@@ -14,6 +14,6 @@ public class Jngfjkngfgf {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JE METS DU CACA ICI!
     }
 }

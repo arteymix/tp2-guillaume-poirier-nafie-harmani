2d5ffe6659f091c378b34acce6b9330e8bfb1c37
nafie_story <nafie_story@hotmail.com>
2011-11-10 19:16:28 +0000

    Ovni mouvement Image bank img

--- src/content/ImageBank.java
+++ src/content/ImageBank.java
@@ -5,6 +5,7 @@
 package content;
 
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -15,9 +16,17 @@ import javax.imageio.ImageIO;
 public class ImageBank {
     
     public final Image canon0;
-
+  public  final Image CANON1 = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image CANON2 = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image ENEMI = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image ENEMIOR = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image SUPERSONIC = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image SUPERSONICOR = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image BOSS1 = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image BOSS2 = Toolkit.getDefaultToolkit().getImage("");
+    public  final Image BOSS3 = Toolkit.getDefaultToolkit().getImage("");
     public ImageBank() throws IOException {
-    canon0 = ImageIO.read((ClassLoader.getSystemResourceAsStream("canon0.jpg")));
+    canon0 = ImageIO.read((ClassLoader.getSystemResourceAsStream("content/canon0.jpg")));
 
     }
 

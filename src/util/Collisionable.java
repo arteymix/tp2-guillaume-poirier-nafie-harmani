4d9f3b0ea4ce3ed3886fbb9fae52199275f7438a
guillaumepoiriermorency <guillaumepoiriermorency@gmail.com>
2011-11-11 13:20:17 +0000

    Ajout de la méthode getDommage() dans Collisionable, qui retourne les dommages d'un objet Collisionnable lors d'une collision afin de simplifier le calcul des .. euh.. dommages! Le canon a subi quelques améliorations mineures GameValues doit être utilisé pour autant de valeurs que possible, si cela est bien fait, il pourra éventuellement servir d'objet sérialisable.

--- src/util/Collisionable.java
+++ src/util/Collisionable.java
@@ -10,17 +10,23 @@ import java.awt.Rectangle;
  *
  * @author Guillaume Poirier-Morency
  */
-public interface Collisionable{
+public interface Collisionable {
+
     /**
      * Retourne le Rectangle de l'objet Collisionable.
      * @return 
      */
     public Rectangle getRectangle();
+
     /**
      * Provoque une collision de l'objet courant avec un autre objet Collisionable.
      * @param c est l'objet Collisionable qui a provoqué la collision.
      */
     public void collision(Collisionable c);
 
-    
+    /**
+     * Méthode qui obtient les dommage qu'un objet collisionable cause lors d'une collision.
+     * @return les dommages que l'objet collisionnable cause lors d'une collision.
+     */
+    public int getDommage();
 }

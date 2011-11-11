c5dc0d1f4cb47b30a593a85e781f497a6775889d
guillaumepoiriermorency <guillaumepoiriermorency@gmail.com>
2011-11-11 13:30:59 +0000

    Suppression de GameValues, je ne veux pas disperser les variables du jeu, mais travailler localement dans chaque objets. La sérialisation sera considérée plus tard...

--- src/graphique/Nuage.java
+++ src/graphique/Nuage.java
@@ -13,7 +13,7 @@ import main.Main;
  * @author Guillaume Poirier-Morency
  */
 public final class Nuage extends Dessinable implements Serializable {
-
+public static final int PROBABILITE_APPARITION_NUAGE = 1000;
     private Vecteur position = new Vecteur(0, (new Random()).nextInt(100) + 50);
     Image img = Main.imageBank.nuage;
 

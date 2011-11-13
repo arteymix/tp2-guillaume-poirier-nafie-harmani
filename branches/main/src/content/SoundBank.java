d3ab6b03b66482957d780b856e64929845336e19
guillaumepoiriermorency@gmail.com <guillaumepoiriermorency@gmail.com@88543d1f-1afb-2b75-7ae9-38cb9dc2246a>
2011-11-13 02:33:58 +0000

    API pour le son en préparation Les images et les sons sont sous-packaged dans content.images et content.sons respectivement Les images ont été déplacés dans leurs sous packages respectifs Le SoundManager, serveur interne de son, est un Thread implémenté dans le Main

---
+++ branches/main/src/content/SoundBank.java
@@ -0,0 +1,20 @@
+package content;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioFormat.Encoding;
+import javax.sound.sampled.AudioInputStream;
+
+/**
+ * Banque de sons pour le TP2. Les sons sont statiques pour une question 
+ * pratique : il n'est pas nécéssaire d'instancier la classe pour y accéder, ce 
+ * qui reviendrait à la même chose.
+ * Les sons fonctionnent pas threads internes avec l'os.
+ * Des méthodes statiques seront crée afin d'utiliser les sons correctement. 
+ * @author Guillaume Poirier-Morency
+ */
+public class SoundBank {
+public SoundBank() {
+
+}
+    public final static AudioInputStream MISSILE = new AudioInputStream(ClassLoader.getSystemResourceAsStream("content/sounds/nuage.png"), new AudioFormat(new Encoding(""), 10.0f, 1, 1, 2, 1.2f, true), 10);
+}

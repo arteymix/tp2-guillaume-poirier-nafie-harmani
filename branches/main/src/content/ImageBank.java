5cd3e1270d8c54386873785476f9c4d2e250dcb2
nafie_story@hotmail.com <nafie_story@hotmail.com@88543d1f-1afb-2b75-7ae9-38cb9dc2246a>
2011-11-14 06:24:33 +0000

    Animation enemi, tank en gif (Faudrait trouvrait une maniere pour les animes) Le back ground du stage 1 Dessinable extends Applette

--- branches/main/src/content/ImageBank.java
+++ branches/main/src/content/ImageBank.java
@@ -1,5 +1,6 @@
 package content;
 
+import java.applet.Applet;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import javax.imageio.ImageIO;
  * Fichier d'objet pour la banque d'images.
  * @author Guillaume Poirier-Morency
  */
-public class ImageBank implements Serializable {
+public class ImageBank extends Applet implements Serializable {
 
     public final Image canon0;
     public final Image nuage;
@@ -29,6 +30,6 @@ public class ImageBank implements Serializable {
     public ImageBank() throws IOException {
         nuage = ImageIO.read((ClassLoader.getSystemResourceAsStream("content/images/nuage.png")));
         missile = ImageIO.read((ClassLoader.getSystemResourceAsStream("content/images/missile.png")));
-        canon0 = ImageIO.read((ClassLoader.getSystemResourceAsStream("content/images/canon0.jpg")));
+        canon0 = ImageIO.read((ClassLoader.getSystemResourceAsStream("content/images/submarine1.gif")));
     }
 }

ae5502fa7f60121e56dca0cfec689ae10c061a2f
guillaumepoiriermorency@gmail.com <guillaumepoiriermorency@gmail.com@88543d1f-1afb-2b75-7ae9-38cb9dc2246a>
2011-11-16 12:24:20 +0000

    Le canon est corrigé et ne tire plus en fou.

--- branches/main/src/graphique/Projectile.java
+++ branches/main/src/graphique/Projectile.java
@@ -57,6 +57,8 @@ public class Projectile extends Dessinable implements Collisionable, Serializabl
 
     @Override
     public void dessiner(Graphics g) {
+        
+        
         if (position.y > MainCanvas.canvasSize.y | position.x < 0 | position.x > MainCanvas.canvasSize.x) {
             this.isDessinable = false;
         }

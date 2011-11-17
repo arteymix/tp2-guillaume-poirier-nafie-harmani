4f57186c33583e7d391530f8b1df003d22b90beb
nafie_story <nafie_story@hotmail.com>
2011-11-17 18:12:02 +0000

    vgv

---
+++ src/javaapplication72/JavaApplication72.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication72;
+
+/**
+ *
+ * @author Usager
+ */
+public class JavaApplication72
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)    
+    {
+        // TODO code application logic here
+    }
+}

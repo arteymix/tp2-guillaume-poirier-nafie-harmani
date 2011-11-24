4416d72aa2cfbfd614b7df6800b87d76b464de92
guillaumepoiriermorency <guillaumepoiriermorency@gmail.com>
2011-11-24 20:08:38 +0000

    Hah!

---
+++ src/com/android/topyoutubequebec/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.topyoutubequebec;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+private TabHost th;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+       
+        //this.setContentView(th);
+        super.onCreate(savedInstanceState);    
+        
+        
+        
+        
+        
+
+
+        
+        this.setTitle("Top Youtube Québec");
+
+    }
+
+   
+}

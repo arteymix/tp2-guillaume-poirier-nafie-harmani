f320aeb9dd378188191b5944bd9cef451f864294
guillaumepoiriermorency <guillaumepoiriermorency@gmail.com>
2011-12-06 15:32:25 +0000

    Woohoo

--- branches/main/src/graphique/component/ProjectileEnnemi.java
+++ branches/main/src/graphique/component/ProjectileEnnemi.java
@@ -160,9 +160,12 @@ public final class ProjectileEnnemi extends Dessinable implements Collisionable,
             return;
         }
         if (c instanceof Projectile) {
-            this.isDessinable = false;
+            
             Main.points += 25 * Main.level;
+            Main.tentaculesKilled++;
+            System.out.println("shit");
             Main.composantesDessinables.add(new PointsObtenus((int) position.x, (int) position.y, "" + 25 * Main.level));
+            this.isDessinable = false;
             // On se sert de l'explosion du projectile...
         } else if (c instanceof Canon) {
             if (((Canon) c).NUMERO_DU_CANON == 1 && !Canon.isCanon2ValidTarget) {
